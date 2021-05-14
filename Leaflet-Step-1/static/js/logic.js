3b326cfc5f64958cb09693d09b737fb06523da77
Luis Gomez <luis.g.1990@gmail.com>
2021-05-13 22:15:30 -0500

    Debugging with Dom, found an error in my css.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -7,6 +7,7 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/signif
 // Get request from query URL
 d3.json(queryUrl).then(function(data) {
     // Send the response to a createFeatures function
+    console.log(data);
     createFeatures(data.features);
 });
 
@@ -14,9 +15,10 @@ function createFeatures(earthquakeData) {
 
     // Create the function that walks the features array
     // Creates a popup on each feature describing the time and place of earthquake
-
+    console.log("createFeatures");
     function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3>`);
+        console.log(feature.properties.time);
+        layer.bindPopup(`<h3>${Date(feature.properties.time)}</h3>`);
     }
 
     // Create a GeoJSON layer containing the features array
@@ -31,14 +33,15 @@ function createFeatures(earthquakeData) {
 }
 
 function createMap(earthquakes) {
-
+    console.log("createMap");
+    console.log(earthquakes);
     // Define outdoorsmap and lightmap layers
-    var outdoorsmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
         maxZoom: 18,
         zoomOffset: -1,
-        id: "mapbox/outdoors-v11",
+        id: "mapbox/streets-v11",
         accessToken: API_KEY
     });
     
@@ -51,7 +54,7 @@ function createMap(earthquakes) {
 
     // Define baseMaps object to hold base layers
     var baseMaps = {
-        "Outdoors Map": outdoorsmap,
+        "Street Map": streetmap,
         "Light Map": lightmap
     }
 
@@ -61,12 +64,12 @@ function createMap(earthquakes) {
     };
 
     // Create our map, passing along the outdoors layer and earthquake layer to display as default
-    var myMap = L.map("map-id", {
+    var myMap = L.map("map", {
         center: [
-            37.09, -95.71
+            45, -55
         ],
-        zoom: 5,
-        layers: [outdoorsmap, earthquakes]
+        zoom: 18,
+        layers: [streetmap, earthquakes]
     });
 
     // layer control
